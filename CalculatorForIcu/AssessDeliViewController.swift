3b678e0630b0edf3b81ed56b24e800d2afc42973
Yuji Yamamoto <Yuji@Yuji-no-MacBook-Air.local>
2017-06-27 15:22:13 +0800

    初めてのコミット

---
+++ CalculatorForIcu/AssessDeliViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AssessForRespViewController.swift
+//  CalculatorForIcu
+//
+//  Created by Yuji Yamamoto on 2017/06/18.
+//  Copyright © 2017年 Yuji Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class AssessForRespViewController: UIViewController {
+        var sIndex = -1
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            print("前の画面から選択された行:\(sIndex)")
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+}
