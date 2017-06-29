07ef253886a1753ba7f97bd4f61b68f63bb94c17
Yuji Yamamoto <Yuji@Yuji-no-MacBook-Air.local>
2017-06-29 12:59:20 +0800

    UI作ってまーす。

---
+++ CalculatorForIcu/historyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  historyViewController.swift
+//  CalculatorForIcu
+//
+//  Created by Yuji Yamamoto on 2017/06/28.
+//  Copyright © 2017年 Yuji Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class historyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
