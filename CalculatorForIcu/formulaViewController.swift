2bb56071962a0cebed935ad1a80f8ceffa1b8d39
Yuji Yamamoto <Yuji@Yuji-no-MacBook-Air.local>
2017-07-21 07:35:18 +0800

    レイアウト作業　栄養・DICのズレ修正中

--- CalculatorForIcu/formulaViewController.swift
+++ CalculatorForIcu/formulaViewController.swift
@@ -18,6 +18,18 @@ class formulaViewController: UIViewController {
     @IBAction func coActive(_ sender: UIButton) {
         //部品のアラートを作成
         let alertController = UIAlertController(title: "活動係数", message: "1.0 - 1.1 (寝たきり)\n1.2 (ベッド上安静)\n1.3 (ベッド以外での活動)\n1.5 (やや低い)\n1.7 (適度)\n1.9 (高い)", preferredStyle:  .alert)
+        //messageを左寄せ
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.left
+        
+        let messageText = NSMutableAttributedString(
+            string: "1.0 - 1.1 (寝たきり)\n1.2 (ベッド上安静)\n1.3 (ベッド以外での活動)\n1.5 (やや低い)\n1.7 (適度)\n1.9 (高い)",
+            attributes: [
+                NSParagraphStyleAttributeName: paragraphStyle,
+                NSFontAttributeName: UIFont.systemFont(ofSize: 13.0)
+            ]
+        )
+        alertController.setValue(messageText, forKey: "attributedMessage")
         
         //アラートにOKボタンを追加
         alertController.addAction(UIAlertAction(title: "OK", style:  .default, handler: nil))
@@ -30,6 +42,19 @@ class formulaViewController: UIViewController {
     @IBAction func coStress(_ sender: UIButton) {
         //部品のアラートを作成
         let alertController = UIAlertController(title: "ストレス係数", message:"手術：1.1(軽度),1.2(中等度),1.8(高度)\n外傷：1.35(骨折),1.6(頭部損傷でステロイド使用)\n感染症：1.2(軽度),1.5(中程度)\n熱傷：1.5(体表面積の40％),1.95(体表面積の100％)\nがん：1.1-1.3\n体温：36℃から1℃上昇ごとに0.2増加", preferredStyle:  .alert)
+        
+        //messageを左寄せ
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.left
+        
+        let messageText = NSMutableAttributedString(
+            string: "手術：1.1(軽度),1.2(中等度),1.8(高度)\n \n外傷：1.35(骨折),1.6(頭部損傷でステロイド使用)\n \n感染症：1.2(軽度),1.5(中程度)\n \n熱傷：1.5(体表面積の40％),1.95(体表面積の100％)\n \nがん：1.1-1.3\n \n体温：36℃から1℃上昇ごとに0.2増加",
+            attributes: [
+                NSParagraphStyleAttributeName: paragraphStyle,
+                NSFontAttributeName: UIFont.systemFont(ofSize: 13.0)
+            ]
+        )
+        alertController.setValue(messageText, forKey: "attributedMessage")
         
         //アラートにOKボタンを追加
         alertController.addAction(UIAlertAction(title: "OK", style:  .default, handler: nil))
